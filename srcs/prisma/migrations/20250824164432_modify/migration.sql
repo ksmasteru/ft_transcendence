90cc5b34e41b52f664bf4e399d957539dc4ce6d0
ksmasteru <hichamessakhiyy@gmail.com>
2026-07-27 14:27:56 +0100

    testing deployemnt

--- srcs/prisma/migrations/20250824164432_modify/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "GAMES_userId_key";
-
--- DropIndex
-DROP INDEX "password_resets_userId_key";
--- srcs/prisma/migrations/20251201155049_add_last_read_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "chat_participants" ADD COLUMN "lastReadAt" DATETIME;
